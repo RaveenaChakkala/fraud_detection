6d280a38045600929a1050a1a8442e6a13298bcd
RaveenaChakkala <raveena.chakkala@gmail.com>
2022-12-17 00:29:28 +0530

    made chanegs to model code and model2 is better model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print("adding all ML related code here")
+
+def model1():
+    print("calculating model1")
+
+def model2():
+    print("calculating model2")
+
+def model3():
+    print("calculating model3")
+
+Print("model 2 is proven to be a better model")
